b1db62e01fe0fd295d372699179bca7fd7929431
olegsameluk <olegsameluk82@gmail.com>
2023-07-18 15:17:13 +0300

    fix: display small button margin

--- src/components/landingComponents/Button/Button.tsx
+++ src/components/landingComponents/Button/Button.tsx
@@ -1,8 +1,8 @@
 const Button = ({ text }: { text: string }) => {
   return (
     <button
-      className="rounded-2xl bg-light-blue p-4 font-bold text-white sm:text-custom-16/26
-     md:ml-2 md:text-custom-16/26 lg:text-custom-20/26 "
+      className="rounded-2xl bg-light-blue p-4 font-bold text-white sm:ml-2
+     sm:text-custom-16/26 md:text-custom-16/26 lg:text-custom-20/26 "
     >
       {text}
     </button>
