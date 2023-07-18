3bf423aebbfe4b0aabd417621636c503c6ea8ed0
olegsameluk <olegsameluk82@gmail.com>
2023-07-18 15:23:04 +0300

    fix: display small comments height

--- src/components/landingComponents/Comments/Comment.tsx
+++ src/components/landingComponents/Comments/Comment.tsx
@@ -7,7 +7,7 @@ const Comment = ({ ...prop }: IComment) => {
         prop.index === 0
           ? 'bg-light-blue text-white mobile:-ml-14 md:-ml-10 md:mr-30px  lg:-ml-52'
           : 'ml-30px bg-white mobile:hidden md:ml-10 md:block'
-      } h-[500px] flex-1 rounded px-[50px] py-20 `}
+      } flex-1 rounded px-[50px] py-20 mobile:h-[400px] md:h-[500px]`}
     >
       <div
         className={`h-[50px] w-[50px] ${
@@ -15,7 +15,7 @@ const Comment = ({ ...prop }: IComment) => {
         } bg-no-repeat`}
       />
       <p className=" pt-6 text-custom-18/36">{prop.comment}</p>
-      <p className="absolute bottom-0 pb-20 text-custom-18/36 font-medium">
+      <p className="absolute bottom-0 text-custom-18/36 font-medium mobile:pb-16 md:pb-20">
         <span>{prop.userName} /</span>
         <span className="pl-2.5">{prop.group}</span>
       </p>
