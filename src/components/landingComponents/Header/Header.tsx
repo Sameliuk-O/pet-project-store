e112ea8a9b23e8966fdbece6e62c4529c9b1a352
olegsameluk <olegsameluk82@gmail.com>
2023-07-21 15:49:16 +0300

    test: Login test

--- src/components/landingComponents/Header/Header.tsx
+++ src/components/landingComponents/Header/Header.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import { Navigation } from '../Navigation';
 
 const Header: React.FC = () => {
@@ -6,7 +8,7 @@ const Header: React.FC = () => {
       <div className="container mx-auto">
         <div className="mobile:flex-col mobile:pb-3.5 mobile:pt-6 mobile:text-center sm:flex-col sm:pb-3.5 sm:pt-6 sm:text-center md:flex md:flex-row md:justify-between md:px-10 md:py-9">
           <h1 className="font-sans font-black uppercase leading-8 text-white mobile:pb-3.5 mobile:text-custom-26/58 sm:text-3xl md:text-4xl lg:text-2xl">
-            orange
+            <Link to={'/login'}>orange</Link>
           </h1>
           <div className="flex-row-reverse space-x-4 space-x-reverse">
             <Navigation />
