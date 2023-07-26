f3c0efa6cd1359389b0023d9a908b2a417679175
olegsameluk <olegsameluk82@gmail.com>
2023-07-26 10:11:20 +0300

    feat: add test product card and shopping box

--- src/components/ShoppingBox/ShoppingBox.test.tsx
+++ src/components/ShoppingBox/ShoppingBox.test.tsx
@@ -1,26 +1,64 @@
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { BrowserRouter } from 'react-router-dom';
 
 import { ShoppingBox } from './index';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { deleteProduct } from '../../store/productSlice';
 import { renderWithProviders } from '../../utils/test-utils';
+
+jest.mock('hooks', () => ({
+  useAppDispatch: jest.fn(),
+  useAppSelector: jest.fn(),
+}));
+
+const mockProductCard = {
+  product: [
+    {
+      date: '2023-07-24',
+      productInfo: {
+        image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+        price: 109.95,
+        title: 'Mens Cotton Jacket',
+      },
+      products: {
+        productId: 3,
+        quantity: 1,
+      },
+      userId: 1,
+    },
+  ],
+};
 
 beforeEach(() => {
   renderWithProviders(<ShoppingBox counter={1} />, {
     wrapper: BrowserRouter,
   });
+
+  const useAppSelectorUserMock = useAppSelector as jest.Mock;
+  useAppSelectorUserMock.mockReturnValue(mockProductCard);
 });
 
 describe('Shopping box', () => {
+  const useDispatchMocked = useAppDispatch as jest.Mock;
+  useDispatchMocked.mockImplementation(() => deleteProduct(3));
+
   test('in display', () => {
     const counter = screen.getByText('1');
     const imageBox = screen.getByRole('img');
-    screen.debug();
     expect(counter).toBeInTheDocument();
     expect(imageBox).toBeInTheDocument();
   });
 
-  // test('on click open popup', () => {
-  //   const openPopup = screen.getByTestId('openPopup');
-  //   fireEvent.click(openPopup);
-  // });
+  test('on click open popup', async () => {
+    const openPopup = screen.getByTestId('openPopup');
+    await userEvent.click(openPopup);
+    const closePopup = screen.getByRole('button', { name: 'X' });
+    expect(closePopup).toBeInTheDocument();
+    const deleteButton = screen.getByRole('button', { name: 'Delete' });
+    expect(deleteButton).toBeInTheDocument();
+    await userEvent.click(deleteButton);
+    await userEvent.click(closePopup);
+    expect(closePopup).not.toBeInTheDocument();
+  });
 });
